dec26b3db390fe4c6e0da0089908662a2df04f67
Erwan Lemaire <erwan_lemaire@yahoo.fr>
2025-04-01 12:48:06 +0200

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+Hooks.on("ready", () => {
+    var logo = document.getElementById("logo");
+    logo.setAttribute("src", "modules/style_EDC/img/logo_pause_EdC.webp");
+    var pauseImg = document.getElementById("pause");
+
+
+    pauseImg.querySelector("img").setAttribute("src", "modules/style_EDC/img/symbole_EdC.webp");
+
+})
